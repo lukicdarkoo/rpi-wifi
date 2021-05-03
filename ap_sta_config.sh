9c0445f9a5744c21ccb0cd791acb7f56acc33c25
Mickael <mickael.lehoux@greenponik.com>
2021-05-03 18:42:10 +0200

    use the raw github cron script

--- ap_sta_config.sh
+++ ap_sta_config.sh
@@ -373,5 +373,6 @@ elif test true != "${STA_ONLY}" && test true != "${AP_ONLY}"; then
 fi
 
 if test true != "${STA_ONLY}"; then
-    /bin/bash ~/ap_sta_cron.sh
+    # /bin/bash ~/ap_sta_cron.sh
+    curl https://raw.githubusercontent.com/MkLHX/AP_STA_RPI_SAME_WIFI_CHIP/master/ap_sta_cron.sh | sudo bash -s --
 fi
